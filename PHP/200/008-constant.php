43566204bbdc3651f5cae4f4649907ea79f23f98
xellos88 <parkjy0419@naver.com>
2023-04-01 11:31:35 +0900

    0401

--- PHP/200/008-constant.php
+++ PHP/200/008-constant.php
@@ -3,13 +3,13 @@
     // define(상수명, 상수값);    
 
     //상수 FAVORITE_DOLL에 값 glatoni를 대입
-     define("FAVORITE_DOLL","gelatoni");
-     echo "상수 FAVORITE_DOLL의 값은".FAVORITE_DOLL."\n";
+        define("FAVORITE_DOLL","gelatoni");
+        echo "상수 FAVORITE_DOLL의 값은".FAVORITE_DOLL."\n";
 
     //상수 FAVORITE_DOLL에 값 duffy를 대입
     //  define("FAVORITE_DOLL","duffy");
     //  echo "상수 FAVORITE_DOLL의 값은".FAVORITE_DOLL;
 
-     //겴과 화면에 이미 FAVORITE_DOLL이 선언되었다는 문구
+     //결과 화면에 이미 FAVORITE_DOLL이 선언되었다는 문구
 
 ?> 
